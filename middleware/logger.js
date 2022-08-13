59fac07c059fb22d805318690bfd7d51ef0bc617
saba2003 <sabamagala405@gmail.com>
2022-08-13 18:15:05 +0400

    day 1 complete

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+// @desc    Logs request to console
+const logger = (request, response, next) => {
+    console.log(
+        `${request.method} ${request.protocol}://${request.get('host')}${request.originalUrl}`
+    );
+    next()
+}
+
+module.exports = logger
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
@@ -8,6 +9,12 @@ const bootcamps = require('./routes/bootcamps')
 dotenv.config({ path: './config/config.env' })
 
 const application = express()
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    application.use(morgan('dev'))
+}
+
 
 // Mount routers
 application.use('/api/v1/bootcamps', bootcamps)
